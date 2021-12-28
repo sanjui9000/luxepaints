299da66bda6c137de83139e7b781622fac6a0a29
sanjui9000 <sanjui9000@gmail.com>
2021-12-27 20:26:00 -0800

    Updates

---
+++ precache-manifest.33ee6e728e15530ad500a90798695de2.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb91b0d76c88289bb5e021463a8fafa",
+    "url": "/luxepaints/index.html"
+  },
+  {
+    "revision": "ad565415bf86274ac7b1",
+    "url": "/luxepaints/static/css/2.b5cd4b83.chunk.css"
+  },
+  {
+    "revision": "63dea67449c79412f579",
+    "url": "/luxepaints/static/css/main.e0a4c4a6.chunk.css"
+  },
+  {
+    "revision": "ad565415bf86274ac7b1",
+    "url": "/luxepaints/static/js/2.6717a484.chunk.js"
+  },
+  {
+    "revision": "6ac04bf051666d18b04b71098001435f",
+    "url": "/luxepaints/static/js/2.6717a484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63dea67449c79412f579",
+    "url": "/luxepaints/static/js/main.0d83e637.chunk.js"
+  },
+  {
+    "revision": "e12ffda02140791b1dbf",
+    "url": "/luxepaints/static/js/runtime-main.a258c990.js"
+  },
+  {
+    "revision": "984bd4e1944fda4cfeba52bef992f7ba",
+    "url": "/luxepaints/static/media/AboutArticleImage.984bd4e1.jpg"
+  },
+  {
+    "revision": "acd374059e9e9a8fd852393230f1010e",
+    "url": "/luxepaints/static/media/AboutUsImage.acd37405.jpg"
+  },
+  {
+    "revision": "79b6167962ee95a91f64e8bd218d6fec",
+    "url": "/luxepaints/static/media/Aeonik-Medium.79b61679.otf"
+  },
+  {
+    "revision": "844324dc83d98980285a9ab8f3dcb5f0",
+    "url": "/luxepaints/static/media/Aeonik-Regular.844324dc.otf"
+  },
+  {
+    "revision": "b65aab45c2788ec87d54ddcd6724c4ec",
+    "url": "/luxepaints/static/media/ColorsArticleImage.b65aab45.jpg"
+  },
+  {
+    "revision": "9813b5cc719e06598e236aa67cf3a7db",
+    "url": "/luxepaints/static/media/ColorsPageImage.9813b5cc.jpg"
+  },
+  {
+    "revision": "e5f38ae6ae3c7dc442edffa52469ee8c",
+    "url": "/luxepaints/static/media/ColorsProductImage2.e5f38ae6.jpg"
+  },
+  {
+    "revision": "59fdfb9e2478640cb1324eb62936d7db",
+    "url": "/luxepaints/static/media/ColorsProductsImage.59fdfb9e.jpg"
+  },
+  {
+    "revision": "e322211299c4b2087adb87715ca1d00b",
+    "url": "/luxepaints/static/media/ContactUsImage.e3222112.jpg"
+  },
+  {
+    "revision": "26a6a8c6927c438050243da3bb432f32",
+    "url": "/luxepaints/static/media/EmulsionsImage.26a6a8c6.jpg"
+  },
+  {
+    "revision": "c7a11552e8a2685c44d59739b65789fa",
+    "url": "/luxepaints/static/media/HomePageImage.c7a11552.jpg"
+  },
+  {
+    "revision": "600b7bdabda659bf9e181191357d4509",
+    "url": "/luxepaints/static/media/MineralsProductImage.600b7bda.jpg"
+  },
+  {
+    "revision": "8291b2e2a0871b3d78726be44d788708",
+    "url": "/luxepaints/static/media/PaintTools.8291b2e2.jpg"
+  },
+  {
+    "revision": "9598ba90eac180756c0e258c7f959595",
+    "url": "/luxepaints/static/media/ProductPageImage.9598ba90.jpg"
+  },
+  {
+    "revision": "eb9585d98738be74e94300ba5177b817",
+    "url": "/luxepaints/static/media/ProductsPageImage.eb9585d9.jpg"
+  },
+  {
+    "revision": "babaf12d7bfd64257dbbf6d4ec287cd8",
+    "url": "/luxepaints/static/media/ProductsRoadMarking.babaf12d.jpg"
+  },
+  {
+    "revision": "d90060a41d1a005981ddab95f46cead0",
+    "url": "/luxepaints/static/media/ServicesArticleImage.d90060a4.jpg"
+  },
+  {
+    "revision": "0205db484413cd6ca61cae784a21399a",
+    "url": "/luxepaints/static/media/ServicesImage.0205db48.jpg"
+  },
+  {
+    "revision": "3a9496f1c567db44f255299bc8a18b42",
+    "url": "/luxepaints/static/media/TexturePaintsProductsImage.3a9496f1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a71e2627a40688ce952197fb66f4951b.js"
+  "/luxepaints/precache-manifest.33ee6e728e15530ad500a90798695de2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/luxepaints/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
